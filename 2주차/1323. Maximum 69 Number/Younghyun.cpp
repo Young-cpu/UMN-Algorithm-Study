308248e6ef84ea7b4095cf696c40d025042fd89e
Younghyun Jung <77591817+Young-cpu@users.noreply.github.com>
2021-09-18 19:44:32 -0500

    Update Younghyun.cpp

--- 2주차/1323. Maximum 69 Number/Younghyun.cpp
+++ 2주차/1323. Maximum 69 Number/Younghyun.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    int maximum69Number (int num) {
+        vector<int> nums;
+        while (num != 0){
+            nums.push_back(num % 10);
+            num = num / 10;
+        }
+        
+        for (int i = nums.size() - 1; i >=0; i--){
+            if(nums[i] == 6){
+                nums[i] = 9;
+                break;
+            }
+        }
+        
+        int output = 0;
+        
+        for (int i = nums.size() - 1; i >=0; i--){
+            output += nums[i] * pow(10, i);
+        }
+        
+        return output;
+    }
+};
